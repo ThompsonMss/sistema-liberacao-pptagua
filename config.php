9e71f7524235ed8cc8ec7db6659b197ed1bce754
Thompson Silva <thompson.msds@gmail.com>
2020-09-12 14:04:02 -0300

    alter id

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	try {
 		$user = "grj";
 		$pass = "12345grj";
-    	$conn = new PDO('mysql:host=192.168.2.100;dbname=ppliberacao', $user, $pass);
+    	$conn = new PDO('mysql:host=192.168.3.100;dbname=ppliberacao', $user, $pass);
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (Exception $e) {
 		echo 'ERROR: ' . $e->getMessage();
